53f86797816a7e4aa32022f6de9e0bf76419a320
yeonann <lkim82@naver.com>
2023-03-29 17:22:14 +0900

    엔티티

---
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @ManyToOne
+    private Question question;
+}
